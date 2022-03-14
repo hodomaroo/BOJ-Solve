ce6e2d015376997caebd4311cca541a72765c935
hodomaroo <61859590+hodomaroo@users.noreply.github.com>
2022-03-15 01:06:11 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 160 ms, Memory: 254792 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.py
@@ -0,0 +1,20 @@
+import sys
+sys.setrecursionlimit(10**5)
+
+def printChar(count : int, symbol : str) -> None:
+    if not count: return
+    print(symbol, end="")
+    printChar(count-1,symbol)
+
+def handleLines(row : int, limit : int) -> None:
+    if row == limit: return
+
+    printChar((limit - 1) // 2 - abs((limit - 1) // 2 - row) + 1, "*")
+    printChar((abs(row - (limit - 1) // 2)) * 2 , " ")
+    printChar((limit - 1) // 2 - abs((limit - 1) // 2 - row) + 1, "*")
+
+    print()
+
+    handleLines(row + 1,limit)
+
+handleLines(0,int(input()) * 2 - 1)
