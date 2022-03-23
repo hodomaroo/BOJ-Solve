e0d5039e63be6ae6ef4e209ac1a921a3f20f6ac8
hodomaroo <61859590+hodomaroo@users.noreply.github.com>
2022-03-23 20:45:14 +0900

    [Silver V] Title: BABBA, Time: 108 ms, Memory: 123316 KB -BaekjoonHub

--- 백준/Silver/9625. BABBA/BABBA.py
+++ 백준/Silver/9625. BABBA/BABBA.py
@@ -1,9 +1,23 @@
 n = int(input())
 #초기값은 A
+
 dp = [[0] * 2 for _ in range(n + 1)]
 dp[0][0] = 1
 
+"""
+Bottom Up
 for i in range(1,n+1):
     dp[i][0] = dp[i-1][1]
     dp[i][1] = sum(dp[i - 1])
 print(*dp[i])
+"""
+#"""
+def TopDown(n):
+    if n == 0: return [1,0]
+    v1,v2 = TopDown(n-1)
+
+    dp[n][0] = v2
+    dp[n][1] = v1 + v2
+    return dp[n]
+#"""
+print(*TopDown(n))
