796d8b02a23a8d7b541af569681a6b64c9b11f30
hodomaroo <61859590+hodomaroo@users.noreply.github.com>
2022-03-29 13:39:47 +0900

    [Gold V] Title: 골목 대장 호석 - 기능성, Time: 108 ms, Memory: 123316 KB -BaekjoonHub

---
+++ 백준/Gold/20168. 골목 대장 호석 － 기능성/골목 대장 호석 － 기능성.py
@@ -0,0 +1,35 @@
+from heapq import heappop,heappush
+
+n,m,a,b,c = map(int,input().split())
+graph = [[] for _ in range(n+1)]
+
+
+for _ in range(m):
+    f,t,cost = map(int,input().split())
+    graph[f].append((t,cost))
+    graph[t].append((f,cost))
+
+def dijkstra(node,target):
+    heap = []
+    minShame = [[float("inf"), float("inf")] for _ in range(n + 1)]
+
+    heappush(heap,(0,0,node))   #현재 노드 / 수치심 / 사용 비용
+    minShame[node] = [0,0]
+
+    while heap:
+        shame,cost,node = heappop(heap)
+        if [shame,cost] > minShame[node]: continue
+        if node == target:
+            print(shame)
+            exit()
+
+        for nextnode,nextcost in graph[node]:
+            resultShame = max(shame,nextcost)
+            resultCost = cost + nextcost
+
+            if resultCost > c or [resultShame,resultCost] >= minShame[nextnode]: continue
+
+            minShame[nextnode] = [resultShame,resultCost]
+            heappush(heap,(resultShame,resultCost,nextnode))
+dijkstra(a,b)
+print(-1)
