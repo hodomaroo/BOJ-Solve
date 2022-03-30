cc229a212056253567847249b362253961f51937
hodomaroo <61859590+hodomaroo@users.noreply.github.com>
2022-03-30 21:35:51 +0900

    [Silver V] Title: 돌 게임, Time: 112 ms, Memory: 123316 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.py
@@ -0,0 +1,21 @@
+import sys
+sys.setrecursionlimit(1000)
+
+n = int(input())
+dp = [[-1] * 2 for _ in range(n+1)]
+#select = [1,3,4]
+select = [1,3]
+
+#dp[stone][turn] --> 돌이 stone개 있을 때 0 : SK / 1 : CY의 승리 가능 여부
+def gameTheory(stone : int, turn : int) -> bool:
+    if dp[stone][turn] != -1: return dp[stone][turn]
+    if stone == 0:  #남은 돌이 없으면 상대가 승리함
+        return 0
+
+    for number in select:
+        if stone - number < 0: break
+        dp[stone][turn] = max(dp[stone][turn],not gameTheory(stone-number,not turn))
+
+    return dp[stone][turn]
+#print(["CY","SK"][gameTheory(n,0)])
+print(["CY","SK"][n%2])
