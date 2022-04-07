e6c1afd03ea1f6e6deb6a469de07619e14192599
hodomaroo <61859590+hodomaroo@users.noreply.github.com>
2022-04-07 14:38:18 +0900

    [Silver III] Title: 숫자 더하기, Time: 104 ms, Memory: 123316 KB -BaekjoonHub

--- 백준/Silver/9440. 숫자 더하기/숫자 더하기.py
+++ 백준/Silver/9440. 숫자 더하기/숫자 더하기.py
@@ -1,34 +1,29 @@
-from typing import List
 import sys
 input = sys.stdin.readline
 
-def convertListToInt(nums : List) -> int:
-    res = 0
-    for v in nums:
-        res = res * 10 + v
-
-    return res
-
 while True:
-    n,*nums = list(map(int,input().split()))
-    if len(nums) == 0: exit()
+    n,*nums = list(input().split())
+    if len(nums) == 0: break
 
     numsWithoutZero = []
     zeroCnt = 0
 
     for v in nums:
-        if not v: zeroCnt += 1
+        if v == "0": zeroCnt += 1
         else: numsWithoutZero.append(v)
 
     numsWithoutZero.sort(reverse=True)
+    #print(numsWithoutZero)
     lists = [[] for _ in range(2)]
 
     convert = False
     while zeroCnt or numsWithoutZero:
         if lists[convert] and zeroCnt:
-            lists[convert].append(0)
+            lists[convert].append("0")
             zeroCnt -= 1
         else:
             lists[convert].append(numsWithoutZero.pop())
         convert = not convert
-    print(convertListToInt(lists[convert]) + convertListToInt(lists[not convert]))
+
+    #print(lists)
+    print(int("".join(lists[convert])) + int("".join(lists[not convert])))
