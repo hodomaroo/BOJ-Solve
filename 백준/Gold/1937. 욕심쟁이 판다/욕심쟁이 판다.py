ac45f25165b758c6d4fb66f158d108dd2504c798
hodomaroo <61859590+hodomaroo@users.noreply.github.com>
2022-05-05 00:54:25 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 500 ms, Memory: 485980 KB -BaekjoonHub

--- 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.py
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.py
@@ -5,9 +5,11 @@ graph = [list(map(int,input().split())) for _ in range(n)]
 visited = [[-1]*n for _ in range(n)]
 
 dx,dy = [0,0,1,-1],[1,-1,0,0]
+
+
 def in_range(x,y):
     return 0<=x<n and 0<=y<n
-
+"""
 def dfs(x,y):
     global max_value
     if visited[x][y] != -1: return visited[x][y] # 적어도 한번은 방문 -->
@@ -26,3 +28,43 @@ for i in range(n):
         if visited[i][j] == -1:
             max_value = max(dfs(i,j),max_value)
 print(max_value+1)
+"""
+#위상정렬
+
+from collections import deque
+indegree = [[0] * n for _ in range(n)] #--> 진입차수 --> 자기로 들어오는 애 == 자기 자식 수
+link = [[[] for _ in range(n)] for _ in range(n)]
+visited = [[1]*n for _ in range(n)]
+for x in range(n):
+    for y in range(n):
+        for d in range(4):
+            nx, ny = x + dx[d], y + dy[d]
+
+            if in_range(nx,ny) and graph[x][y] < graph[nx][ny]: # --> 제일 작은놈에서 시작해야 --> 부모를 갱신할 수 있음
+                link[x][y].append((nx,ny))
+                indegree[nx][ny] += 1
+
+queue = deque()
+ans = 1
+for i in range(n):
+    for j in range(n):
+        if not indegree[i][j]:
+            queue.append((i,j))
+
+while queue:
+    x,y = queue.popleft()
+
+    for nx,ny in link[x][y]:
+        visited[nx][ny] = max(visited[nx][ny],visited[x][y] + 1)
+        indegree[nx][ny] -= 1
+
+        if indegree[nx][ny] == 0: #--> 부모 노드의 indegree가 0이 됨 --> 자식들의 부모에 대한 갱신이 끝남
+            queue.append((nx,ny))
+            ans = max(ans,visited[nx][ny])
+#print(*visited,sep="\n")
+print(ans)
+
+
+
+
+
