f39e59942b16fdc3d5f493b315822fe8df23a4d4
hodomaroo <61859590+hodomaroo@users.noreply.github.com>
2022-06-05 20:43:15 +0900

    [Gold III] Title: 공주님의 정원, Time: 616 ms, Memory: 132316 KB -BaekjoonHub

---
+++ 백준/Gold/2457. 공주님의 정원/공주님의 정원.py
@@ -0,0 +1,39 @@
+n = int(input())
+flowers = []
+dateDict = {}
+
+for v in [1,3,5,7,8,10,12]:
+    dateDict[v] = 31
+
+for v in [4, 6, 9, 11]:
+    dateDict[v] = 30
+
+dateDict[2] = 28
+
+for _ in range(n):
+    inform = list(map(int, input().split()))
+    inform[:2] = max([3, 1], inform[:2])
+    inform[2:] = min([11, 31], inform[2:])
+    flowers.append(inform)
+
+flowers.sort()
+stack = []
+
+
+for data in flowers:
+    #구간을 이을 수 없는 경우 즉시 종료
+
+    if stack and stack[-1][2:] < data[:2]:
+        print(0)
+        exit()
+
+    if not stack or stack[-1][2:] < data[2:]: #구간 확장이 가능한 경우
+        #치환 가능한 구간을 제거 (구간 시작은 동일하고 폭이 더 넓은경우 이전 구간 치환 가능)
+        while stack and stack[-1][:2] >= data[:2] and stack[-1][2:] <= data[2:]:
+            stack.pop()
+
+        stack.append(data if not stack else (stack[-1][2:] + data[2:]))
+
+ans = len(stack)
+#print(stack)
+print(ans if stack[0][:2] == [3,1] and stack[-1][2:] == [11,31] else 0)
