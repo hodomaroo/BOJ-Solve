f36a2e913d925f6eb4038b0cd5d50288b288dea5
hodomaroo <61859590+hodomaroo@users.noreply.github.com>
2022-06-05 21:36:52 +0900

    [Gold IV] Title: 세 수의 합, Time: 232 ms, Memory: 195756 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.py
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.py
@@ -1,14 +1,9 @@
 nums = sorted([int(input()) for _ in range(int(input()))], reverse=True)
 numSet = set(nums)
-ans = 0
 plusSet = {nums[i] + nums[j] for i in range(len(nums)) for j in range(i, len(nums))}
-minusSet = {nums[j] - nums[i] for i in range(len(nums)) for j in range(i+1)}
-multiSet = plusSet & minusSet
 
-ans = 0
-#print(plusSet,minusSet)
-for v in nums: #maximum 1000
-    for twoSum in multiSet:
-        if v - twoSum in numSet:
-            print(v)
+for i in range(len(nums)):
+    for j in range(i,len(nums)):
+        if nums[i] - nums[j] in plusSet:
+            print(nums[i])
             exit()
