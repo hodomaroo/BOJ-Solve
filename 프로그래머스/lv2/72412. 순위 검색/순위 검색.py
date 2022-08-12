32f9eed8656d940c5aa366a55491cf84d01c657b
hodomaroo <61859590+hodomaroo@users.noreply.github.com>
2022-08-12 10:57:01 +0900

    [level 2] Title: 순위 검색, Time: 2463.35 ms, Memory: 53.9 MB -BaekjoonHub

--- 프로그래머스/lv2/72412. 순위 검색/순위 검색.py
+++ 프로그래머스/lv2/72412. 순위 검색/순위 검색.py
@@ -1,53 +1,43 @@
-from bisect import bisect_left
-
+from bisect import bisect_right
 
 class Node:
     def __init__(self):
         self.child = {}
         self.val = []
-        self.sorted = False
-
 
 def solution(info, query):
-    datas = [["java", "cpp", "python"], ["backend", "frontend"],["senior","junior"], ["chicken", "pizza"]]
-
-    index = [3, 2, 2]
-
-    def insert(node: Node, inform: list, depth: int):
-        if depth == 4:
-            node.val.append(int(inform[-1]))
-            #print(node.val)
-            return
-
+    datas = [["java","cpp","python"],["backend","frontend"],["chicken","pizza"]]
+    
+    index = [3,2,2]
+    
+    def insert(node : Node, inform : list, depth : int):
+        if depth == 3:
+            node.val.append(inform[-1])
+            print(node.val)
+            return 
+        
         if inform[depth] not in node.child:
             node.child[inform[depth]] = Node()
         insert(node.child[inform[depth]], inform, depth + 1)
-
-    def search(node: Node, inform: list, depth):
-        #print(inform, depth, node.child)
-        if depth == 4:
-            #print(node.val)
-            #print(inform)
-            return len(node.val) - bisect_left(node.val, int(inform[-1]))
-
+    
+    def search(node : Node, inform : list, depth):
+        if depth == 3:
+            return bisect_right(node.val, inform[-1])
+        
         count = 0
         if inform[depth] == "-":
             for v in datas[depth]:
                 if v not in node.child: continue
                 count += search(node.child[v], inform, depth + 1)
-
-        elif inform[depth] in node.child:
-            count += search(node.child[inform[depth]], inform, depth + 1)
-
+        
         return count
-
-    info = sorted([inform.split() for inform in info], key=lambda x: int(x[-1]))
+    
     root = Node()
     for inform in info:
         insert(root, inform, 0)
-
+    
     answer = []
     for q in query:
-        answer.append(search(root, q.split()[0::2] + [q.split()[-1]], 0))
-
+        answer.append(search(root, q.split()[0::2],0))
+    
     return answer
