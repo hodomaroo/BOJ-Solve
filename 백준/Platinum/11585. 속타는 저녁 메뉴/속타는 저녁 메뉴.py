dbec7e3039df3f76ed9aa297fc5bf687aa71b0be
hodomaroo <61859590+hodomaroo@users.noreply.github.com>
2022-08-27 08:03:13 +0900

    [Platinum V] Title: 속타는 저녁 메뉴, Time: 328 ms, Memory: 193280 KB -BaekjoonHub

---
+++ 백준/Platinum/11585. 속타는 저녁 메뉴/속타는 저녁 메뉴.py
@@ -0,0 +1,36 @@
+import sys
+input = sys.stdin.readline
+
+def ConstFail(target : str) -> list:
+    # fail(x) -> y
+    fail = [0] * (len(target) + 1)
+
+    #0에서는 무조건 Root로 회귀함
+    #1 -> 0번 문자와 같은 경우 이동 가능한 상태
+    for i in range(2, len(target) + 1):
+        fail[i] = fail[i - 1]
+
+        while fail[i] != 0 and target[fail[i]] != target[i - 1]:
+            fail[i] = fail[fail[i]]
+
+        fail[i] += (target[fail[i]] == target[i - 1])
+
+    return fail
+    
+n = int(input().rstrip())
+fail = ConstFail(list(input().rstrip().split()))
+input().rstrip()
+
+"""
+availableList = []
+for i in range(1, len(fail)):
+    if fail[i] and fail[i] < fail[i-1]:
+        availableList = []
+    availableList.append(i)
+availableList.append(i)
+"""
+
+for v in range(1, n + 1):
+    if (n % v == 0 and fail[-1] % v == 0 and n // v == fail[-1] // v + 1) or v == n:
+        print(1,"/",v, sep = "")
+        exit(0)
