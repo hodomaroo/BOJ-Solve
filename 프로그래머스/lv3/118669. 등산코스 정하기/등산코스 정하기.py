a5f79ac93cf89382b7c55988bd37c5065231e25c
hodomaroo <61859590+hodomaroo@users.noreply.github.com>
2022-09-06 23:22:53 +0900

    [level 3] Title: 등산코스 정하기, Time: 552.17 ms, Memory: 89.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/118669. 등산코스 정하기/등산코스 정하기.py
@@ -0,0 +1,34 @@
+from heapq import heappush, heappop
+
+def solution(n, paths, gates, summits):
+    #(cost , nodeValue)
+    nodes = [float("inf") for _ in range(n + 1)]
+    graph = [[] for _ in range(n + 1)]
+    visit = [False] * (n + 1)
+    isSummits = [False] * (n + 1)
+    
+    for a,b,c in paths:
+        graph[a].append((b,c))
+        graph[b].append((a,c))
+        
+    heap = []
+    for i in gates:
+        nodes[i] = 0
+        heappush(heap, (0,i))
+    
+    for i in summits:
+        isSummits[i] = True
+
+    while heap:
+        cost, node = heappop(heap)
+        if visit[node] or isSummits[node]: continue
+        visit[node] = True
+        
+        for nextnode,nextcost in graph[node]:
+            if nodes[nextnode] > max(cost, nextcost):
+                nodes[nextnode] = max(cost, nextcost)
+                heappush(heap, (nodes[nextnode], nextnode))
+    
+    
+    ans = min(summits, key = lambda x : (nodes[x],x))
+    return [ans,nodes[ans]]
