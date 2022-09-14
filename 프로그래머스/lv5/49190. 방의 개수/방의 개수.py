dc2aed3ce2763caf1ad9d260b1aa1ae2abdb4aad
hodomaroo <61859590+hodomaroo@users.noreply.github.com>
2022-09-15 01:31:48 +0900

    [level 5] Title: 방의 개수, Time: 106.61 ms, Memory: 28.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv5/49190. 방의 개수/방의 개수.py
@@ -0,0 +1,32 @@
+from collections import defaultdict
+def solution(arrows):
+    nodeDict = defaultdict(int)
+    
+    dx,dy = [-1,-1,0,1,1,1,0,-1],[0,1,1,1,0,-1,-1,-1]
+    #하 우 상 좌 / 좌하 우하 우상 좌상
+    count = 0
+    
+    #대각선인 경우 교차하는 대각선이 있는지 검사
+    def checkDiagonal(pos : tuple, dir : int) -> bool:
+        x,y = pos
+        #print(x,y,dir)
+        return  (x + dx[dir], y) in nodeDict and nodeDict[(x + dx[dir], y)] & 1 << (dir + [2,6][dir % 4 == 3]) % 8
+        
+    x,y = 0,0
+    
+    for cmd in arrows:
+        nx,ny = x + dx[cmd], y + dy[cmd]
+            
+        #새로운 간선이고, nx,ny가 이미 방문된 점인경우
+        if not nodeDict[(x,y)] & 1 << cmd:
+            if (nx,ny) in nodeDict:  
+                count += 1
+
+            if cmd % 2 and checkDiagonal((x,y),cmd):
+                count += 1
+        
+        nodeDict[(x,y)] |= 1 << cmd
+        nodeDict[(nx,ny)] |= 1 << (cmd + 4) % 8
+        x,y = nx,ny
+        
+    return count
