d8e3045463fb3d1b2f7b177d820de9e9b1b9765a
hodomaroo <61859590+hodomaroo@users.noreply.github.com>
2022-09-22 00:57:54 +0900

    [Platinum V] Title: 다이아몬드 광산, Time: 768 ms, Memory: 170588 KB -BaekjoonHub

--- 백준/Platinum/1028. 다이아몬드 광산/다이아몬드 광산.py
+++ 백준/Platinum/1028. 다이아몬드 광산/다이아몬드 광산.py
@@ -6,17 +6,16 @@ board = [list(map(int,list(input().rstrip()))) for _ in range(n)]
 dp = [[[-1,-1] for _ in range(m + 2)] for _ in range(n + 2)]
 
 ans = 0
-
 for i in range(1,n + 1):
     for j in range(1, m + 1):
         if not board[i - 1][j - 1]: continue
         ans = max(ans,1)
+        
         dp[i][j][0] = dp[i-1][j-1][0] + 1
         dp[i][j][1] = dp[i-1][j+1][1] + 1
         
-        for s in range(ans,min(dp[i][j]) + 1): 
+        for s in range(min(dp[i][j]), ans -1, -1): 
             if dp[i - s][j - s][1] >= s and dp[i-s][j + s][0] >= s:
-                ans = max(ans,s + 1)      
+                ans = max(s + 1,ans)
+                break      
 print(ans)   
-
-        
