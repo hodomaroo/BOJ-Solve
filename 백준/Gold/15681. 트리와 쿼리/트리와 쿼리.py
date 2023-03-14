4e4f39836645a89507d04a715bcf2094d1e951a8
김두호 <61859590+hodomaroo@users.noreply.github.com>
2023-03-14 22:16:05 +0900

    [Gold V] Title: 트리와 쿼리, Time: 456 ms, Memory: 68980 KB -BaekjoonHub

--- 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.py
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.py
@@ -1,25 +1,23 @@
 import sys
-sys.setrecursionlimit(10**5)
+sys.setrecursionlimit(10**5 + 1)
 input = sys.stdin.readline
 
-n,m,q = map(int,input().split())
+n,m,q = map(int,input().rstrip().split())
 dp = [1 for i in range(n+1)]
-visit =[False for _ in range(n+1)]
 graph = [[] for _ in range(n+1)]
 
 for _ in range(n-1):
-    f,t = map(int,input().split())
+    f,t = map(int,input().rstrip().split())
     graph[f].append(t)
     graph[t].append(f)
 
-def dfs(node : int) -> int:
-    visit[node] = True
+def dfs(parent : int, node : int) -> int:
     for nextnode in graph[node]:
-        if visit[nextnode]: continue
-        dp[node] += dfs(nextnode)
+        if nextnode == parent: continue
+        dp[node] += dfs(node, nextnode)
     return dp[node]
 
-dfs(m)
+dfs(-1,m)
 
 for _ in range(q):
-    print(dp[int(input())])
+    print(dp[int(input().rstrip())])
