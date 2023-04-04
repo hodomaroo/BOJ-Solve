85e2cd9a1631a8385e4894b1e5e29f4567a3dde9
김두호 <61859590+hodomaroo@users.noreply.github.com>
2023-04-04 22:40:51 +0900

    [Gold V] Title: 전구와 스위치, Time: 184 ms, Memory: 130132 KB -BaekjoonHub

--- 백준/Gold/2138. 전구와 스위치/전구와 스위치.py
+++ 백준/Gold/2138. 전구와 스위치/전구와 스위치.py
@@ -4,17 +4,17 @@ goal = list(map(int, list(input())))
 
 _ans = 100001
 for i in range(2):
-    curPrev, curGoal, count = prev[:], goal[:], i
+    curPrev, count = prev[:], i
     if i:
         curPrev[0], curPrev[1] = not curPrev[0], not curPrev[1]
 
     for j in range(1, n):
-        if curPrev[j - 1] != curGoal[j - 1]:
+        if curPrev[j - 1] != goal[j - 1]:
             count += 1
             for k in range(j - 1, min(n, j + 2)):
                 curPrev[k] = not curPrev[k]
 
-    if curPrev[-1] == curGoal[-1]:
+    if curPrev[-1] == goal[-1]:
         _ans = min(_ans, count)
 
 print(-1 if _ans == 100001 else _ans)
